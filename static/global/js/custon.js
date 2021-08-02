42f233f0b01dc9b3bc60efa65cd38f619063526b
josiasAnael <josias199749@gmail.com>
2021-08-01 19:44:06 -0600

    Validaciones

--- static/global/js/custon.js
+++ static/global/js/custon.js
@@ -1,4 +1,19 @@
 $(()=>{
     $('.dropdown-trigger').dropdown();
     $('table').DataTable({});
+    $('input:text').keydown(
+        function (event){
+            var letters = /^[A-Za-z]+$/;
+            if(event.key.match(letters))
+              {
+               return true;
+              }
+            else
+              {
+              alert("message");
+              return false;
+              }
+        }
+    )
 })
+
